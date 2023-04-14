add520ca65b1708d47805b1f7cad2ceb8096ee38
chrystian-souza <chrystian.souza.silveira@gmail.com>
2023-04-13 23:51:55 -0300

    commit 1
    construindo a softwere bloco de notas

--- model/bloco_de_notas.py
+++ model/bloco_de_notas.py
@@ -1,8 +1,8 @@
 class Bloco_De_Notas:
 
-    def __init__(self, id, nome_da_nota, data, texto):
-        self.id = id
-        self.nome = nome_da_nota
+    def __init__(self, nome_da_nota, data, texto):
+        #self.id = id
+        self.nome_da_nota = nome_da_nota
         self.data = data
         self.texto = texto
 
--- view/tela_principal.py
+++ view/tela_principal.py
@@ -12,37 +12,51 @@ class MainWindow (QMainWindow):
 
         self.setWindowTitle('Bloco de notas')
 
-        self.lbl_id = QLabel('id')
-        self.txt_id = QLineEdit()
+        #self.lbl_id = QLabel('id')
+        #self.txt_id = QLineEdit()
         self.lbl_nome_da_nota = QLabel('Título da nota')
         self.txt_nome_da_nota = QLineEdit()
         self.lbl_texto = QLabel('Texto')
         self.txt_texto = QLineEdit()
+        self.txt_data = QLineEdit()
+        self.btn_criar = QPushButton('Criar')
 
         layout = QVBoxLayout()
-        layout.addWidget(self.lbl_id)
-        layout.addWidget(self.txt_id)
+        #layout.addWidget(self.lbl_id)
+        #layout.addWidget(self.txt_id)
         layout.addWidget(self.lbl_nome_da_nota)
         layout.addWidget(self.txt_nome_da_nota)
         layout.addWidget(self.lbl_texto)
         layout.addWidget(self.txt_texto)
+        layout.addWidget(self.btn_criar)
 
         self.container = QWidget()
         self.container.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.setCentralWidget(self.container)
         self.container.setLayout(layout)
 
+        self.btn_criar.clicked.connect(self.criar_nota)
+
 
     def criar_nota(self):
         db = DataBase()
 
         nota = Bloco_De_Notas(
-
-
-
-
+            #id = self.txt_id.text(),
+            nome_da_nota = self.txt_nome_da_nota.text(),
+            data = self.txt_data.text(),
+            texto = self.txt_texto.text()
 
         )
+        if self.btn_criar.text() == 'Criar':
+            retorno = db.registar_notas(nota)
+
+            if retorno == 'ok':
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Information)
+                msg.setWindowTitle('Nota Criada')
+                msg.setText('Sua nota foi criada')
+                msg.exec()
 
 
 
